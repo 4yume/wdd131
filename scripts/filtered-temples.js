bb8bf79187181e4130b5486db9996852b7e51db7
4yume <yume84@outlook.jp>
2026-03-26 15:48:20 +0900

    initial change

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,107 @@
+const today = new Date();
+const year = today.getFullYear();
+
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const menuButton = document.querySelector('#menu');
+const nav = document.querySelector('nav ul');
+const title = document.querySelector("header p");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    title.classList.toggle("hide");
+
+    if (menuButton.textContent === "≣") {
+        menuButton.textContent = "X";
+    }
+    else {
+        menuButton.textContent = "≣"
+    }
+});
+
+
+// consists of a sample of temple objects and place the array
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    // Add more temple objects here...
+    {
+        templeName: "Tokyo Japan",
+        location: "Tokyo Japan",
+        dedicated: "1980, October 27",
+        area: 53997,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/tokyo-japan-temple/tokyo-japan-temple-26340-main.jpg"
+    },
+    {
+        templeName: "Madrid Spain",
+        location: "Madrid Spain",
+        dedicated: "1999, March, 19",
+        area: 45800,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/_temp/056-Madrid-Spain-Temple.jpg"
+    },
+    {
+        templeName: "Bangkok Thailand",
+        location: "Bangkok Thailand",
+        dedicated: "2023, October, 22",
+        area: 48525,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/bangkok-thailand-temple/bangkok-thailand-temple-40037-main.jpg"
+    }
+];
